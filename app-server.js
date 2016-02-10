9828bba99d3d4b3888f49e4c8e451613d28b9ed3
Tony Spiro <tspiro@tonyspiro.com>
2016-02-10 14:41:58 -0600

    add express server

---
+++ app-server.js
@@ -0,0 +1,13 @@
+// app-server.js
+var express = require('express')
+var app = express()
+app.set('port', process.env.PORT || 3000)
+app.use(express.static(__dirname))
+var http = require('http').Server(app)
+// Route
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+http.listen(app.get('port'), () => {
+  console.log('Wedding Site listening on ' + app.get('port'))
+})
